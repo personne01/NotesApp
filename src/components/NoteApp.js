72add2f8f1031ea3a76df1b2a749e49d2e210553
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-08-29 20:00:02 +0700

    fix: crash on archive button click

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,67 +1,82 @@
 import React from "react";
 import NoteList from "./NoteList";
-import ArchieveList from "./ArchieveList"
-import Header from "./Header"
+import ArchieveList from "./ArchieveList";
+import Header from "./Header";
 import InputForm from "./InputForm";
 import { getInitialData } from "../utils/data";
 
-class NoteApp extends React.Component{
-    constructor(props){
+class NoteApp extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            allnotes : getInitialData(),
-            notes : getInitialData().filter(dt => dt.archived === false),
-            archieve : getInitialData().filter(dt => dt.archived === true)
-        }
+            allnotes: getInitialData(),
+            notes: getInitialData().filter((dt) => dt.archived === false),
+            archieve: getInitialData().filter((dt) => dt.archived === true),
+        };
         this.onDeleteHandler = this.onDeleteHandler.bind(this);
         this.onAddHandler = this.onAddHandler.bind(this);
         this.onArchiveEventHandler = this.onArchiveEventHandler.bind(this);
     }
 
-    onDeleteHandler(id){
-        const notes = this.state.notes.filter(note => note.id !== id);
-        this.setState({notes});
-        const archieve = this.state.archieve.filter(archieved => archieved.id !== id);
-        this.setState({archieve});
+    onDeleteHandler(id) {
+        const notes = this.state.notes.filter((note) => note.id !== id);
+        this.setState({ notes });
+        const archieve = this.state.archieve.filter(
+            (archieved) => archieved.id !== id
+        );
+        this.setState({ archieve });
     }
 
-    onAddHandler({title, createdAt, body}){
-        this.setState((prevState)=>{
-            return{
-                notes : [
+    onAddHandler({ title, createdAt, body }) {
+        this.setState((prevState) => {
+            return {
+                notes: [
                     ...prevState.notes,
                     {
-                        id : +new Date(),
+                        id: +new Date(),
                         title,
-                        createdAt, 
+                        createdAt,
                         body,
-                        archived : false
-                    }
-                ]
-            }
-        })
+                        archived: false,
+                    },
+                ],
+            };
+        });
     }
-    onArchiveEventHandler({id}){
+
+    onArchiveEventHandler(id) {
         this.setState((prevState) => {
-            const selectedNote = prevState.allnotes.filter((note) => note.id === id)[0];
-            const moveNote = (selectedNote.archived = false);
+            const notesCopy = [...prevState.notes];
+            const selectedNoteIdx = notesCopy.findIndex(
+                (note) => note.id === id
+            );
+
+            const selectedNote = notesCopy.splice(selectedNoteIdx, 1)[0];
+            selectedNote.archived = true;
+
             return {
-                allnotes: [...prevState.allnotes, moveNote],
-            }
-        })
+                notes: [...prevState.notes.filter((note) => note.id !== id)],
+                archieve: [...prevState.archieve, selectedNote],
+            };
+        });
     }
-    // const notes = getInitialData().filter(dt => dt.archived === true);
-    // const archieve = getInitialData().filter(dt => dt.archived === false);
-    render(){
+
+    render() {
         return (
             <div className="note-app">
                 <Header />
                 <InputForm addNote={this.onAddHandler} />
-                <NoteList notes={this.state.notes} onArchive={this.onArchiveEventHandler}  onDelete = {this.onDeleteHandler} />
-                <ArchieveList notes={this.state.archieve} onDelete = {this.onDeleteHandler} />
+                <NoteList
+                    notes={this.state.notes}
+                    onArchive={this.onArchiveEventHandler}
+                    onDelete={this.onDeleteHandler}
+                />
+                <ArchieveList
+                    notes={this.state.archieve}
+                    onDelete={this.onDeleteHandler}
+                />
             </div>
-        )
+        );
     }
-    
 }
 export default NoteApp;
